fbf95151a92dbe29c6cbdbee6e170b09fd65f19f
chinnanaik47 <chinnanaik47@gmail.com>
2025-07-27 23:29:26 +0530

    Update Asus.js

--- Asus.js
+++ Asus.js
@@ -4,14 +4,14 @@
   );
   document.getElementsByClassName("Dropdown")[0].value = 24;
   PopulateGrid(24);
-  console.log("Dropdown value set to 31 and grid populated.");
+  console.log("Dropdown value set to 24 and grid populated.");
 
   var intId = setInterval(function () {
     for (var i = 0; i < 30; i++) {
       var data =
         document.getElementsByClassName("GridviewScrollItem")[i].cells[2]
           .innerHTML;
-      if (data.indexOf("MAHABUBABA") != -1) {
+      if (data.indexOf("Mogallapally Desiltation(2025)") != -1) {
         $("input[type='radio']")[i].click();
         clearInterval(intId);
         break;
