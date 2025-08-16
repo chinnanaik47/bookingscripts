6971cf33091f30e11e34727d419ea892e126f0ac
chinnanaik47 <chinnanaik47@gmail.com>
2025-08-16 09:45:41 +0530

    Update AutoSelectVehiclesWithCounter.js

--- AutoSelectVehiclesWithCounter.js
+++ AutoSelectVehiclesWithCounter.js
@@ -14,7 +14,7 @@
         const vehicles = [
         "",  
         "",              // index 0 unused
-        "TS30T3677",       // count = 2
+        "TS06UB8558",       // count = 2
         "TS30T3599",       // count = 3
         "TS05UC9429",        // count = 4
         "AP26TK2292",       // count = 5
