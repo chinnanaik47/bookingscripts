3949a43cb159478b24ed88b77769cbe1e0d20cc5
chinnanaik47 <chinnanaik47@gmail.com>
2025-08-27 09:58:36 +0530

    Update Hp.js

--- Hp.js
+++ Hp.js
@@ -2,16 +2,16 @@
   console.log(
     "Stock Yard script loaded successfully from GitHub and running..."
   );
-  document.getElementsByClassName("Dropdown")[0].value = 24;
-  PopulateGrid(24);
-  console.log("Dropdown value set to 24 and grid populated.");
+  document.getElementsByClassName("Dropdown")[0].value = 27;
+  PopulateGrid(27);
+  console.log("Dropdown value set to 27 and grid populated.");
 
   var intId = setInterval(function () {
     for (var i = 0; i < 30; i++) {
       var data =
         document.getElementsByClassName("GridviewScrollItem")[i].cells[2]
           .innerHTML;
-      if (data.indexOf("Subbampeta De-Siltation (2025)") != -1) {
+      if (data.indexOf("Beglur/2024/1") != -1) {
         $("input[type='radio']")[i].click();
         clearInterval(intId);
         break;
