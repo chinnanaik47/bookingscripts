b1fcdc06166047c289e0d3756e117f4a026891db
chinnanaik47 <chinnanaik47@gmail.com>
2025-10-01 20:57:00 +0530

    Update PcLeftBrowsersAll.js

--- PcLeftBrowsersAll.js
+++ PcLeftBrowsersAll.js
@@ -2,16 +2,16 @@
   console.log(
     "Stock Yard script loaded successfully from GitHub and running..."
   );
-  document.getElementsByClassName("Dropdown")[0].value = 24;
-  PopulateGrid(24);
-  console.log("Dropdown value set to 24 and grid populated.");
+  document.getElementsByClassName("Dropdown")[0].value = 31;
+  PopulateGrid(31);
+  console.log("Dropdown value set to 31 and grid populated.");
 
   var intId = setInterval(function () {
     for (var i = 0; i < 30; i++) {
       var data =
         document.getElementsByClassName("GridviewScrollItem")[i].cells[2]
           .innerHTML;
-      if (data.indexOf("Kudunuru SBAB5 (2025)") != -1) {
+      if (data.indexOf("MAHABUBABAD SS FINE") != -1) {
         $("input[type='radio']")[i].click();
         clearInterval(intId);
         break;
