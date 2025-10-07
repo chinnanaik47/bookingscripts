13395bbd519d41ef813f7f176690841066ac5b07
chinnanaik47 <chinnanaik47@gmail.com>
2025-10-07 22:05:07 +0530

    Update Hp.js

--- Hp.js
+++ Hp.js
@@ -1,21 +1,15 @@
-(function () {
-  console.log(
-    "Stock Yard script loaded successfully from GitHub and running..."
-  );
-  document.getElementsByClassName("Dropdown")[0].value = 24;
-  PopulateGrid(24);
-  console.log("Dropdown value set to 24 and grid populated.");
+(function() {
+document.getElementsByClassName('Dropdown')[0].value = 31;
+  PopulateGrid(31);
 
-  var intId = setInterval(function () {
-    for (var i = 0; i < 30; i++) {
-      var data =
-        document.getElementsByClassName("GridviewScrollItem")[i].cells[2]
-          .innerHTML;
-      if (data.indexOf("Kudunuru De-Siltation(2025)") != -1) {
+var intId = setInterval(function(){    
+for(var i=0;i<30;i++){
+    var data = document.getElementsByClassName("GridviewScrollItem")[i].cells[2].innerHTML;
+    if( data.indexOf("MAHABUBABAD SS FINE") != -1) {        
         $("input[type='radio']")[i].click();
         clearInterval(intId);
         break;
-      }
     }
-  }, 1010);
+
+}},1500);
 })();
