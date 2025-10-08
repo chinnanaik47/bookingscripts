1921ced04b54ae8e3b9918df5152c2410b726d8f
chinnanaik47 <chinnanaik47@gmail.com>
2025-10-09 00:03:59 +0530

    Update Asus.js

--- Asus.js
+++ Asus.js
@@ -1,21 +1,15 @@
-(function () {
-  console.log(
-    "Stock Yard script loaded successfully from GitHub and running..."
-  );
-  document.getElementsByClassName("Dropdown")[0].value = 24;
+(function() {
+document.getElementsByClassName('Dropdown')[0].value = 24;
   PopulateGrid(24);
-  console.log("Dropdown value set to 24 and grid populated.");
 
-  var intId = setInterval(function () {
-    for (var i = 0; i < 30; i++) {
-      var data =
-        document.getElementsByClassName("GridviewScrollItem")[i].cells[2]
-          .innerHTML;
-      if (data.indexOf("Kudunuru SBAB5 (2025)") != -1) {
+var intId = setInterval(function(){    
+for(var i=0;i<30;i++){
+    var data = document.getElementsByClassName("GridviewScrollItem")[i].cells[2].innerHTML;
+    if( data.indexOf("Gundla Singaram De-Siltation (2025)") != -1) {        
         $("input[type='radio']")[i].click();
         clearInterval(intId);
         break;
-      }
     }
-  }, 1100);
+
+}},1010);
 })();
