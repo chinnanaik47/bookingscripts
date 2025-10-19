fad30e99a76134c906bd247ddb522eaeeaff8eda
chinnanaik47 <chinnanaik47@gmail.com>
2025-10-19 09:52:22 +0530

    Update PcRightBrowsersAll.js

--- PcRightBrowsersAll.js
+++ PcRightBrowsersAll.js
@@ -1,11 +1,11 @@
 (function() {
-document.getElementsByClassName('Dropdown')[0].value = 24;
-  PopulateGrid(24);
+document.getElementsByClassName('Dropdown')[0].value = 27;
+  PopulateGrid(27);
 
 var intId = setInterval(function(){    
 for(var i=0;i<30;i++){
     var data = document.getElementsByClassName("GridviewScrollItem")[i].cells[2].innerHTML;
-    if( data.indexOf("Kudunuru De-Siltation(2025)") != -1) {        
+    if( data.indexOf("Mahadevpur/2024/1	") != -1) {        
         $("input[type='radio']")[i].click();
         clearInterval(intId);
         break;
