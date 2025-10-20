9c2b206059b3b5dbbabd1b9a178d45cf0ce40b51
chinnanaik47 <chinnanaik47@gmail.com>
2025-10-20 09:55:08 +0530

    Update Hp.js

--- Hp.js
+++ Hp.js
@@ -1,11 +1,11 @@
 (function() {
-document.getElementsByClassName('Dropdown')[0].value = 45;
-  PopulateGrid(45);
+document.getElementsByClassName('Dropdown')[0].value = 27;
+  PopulateGrid(27);
 
 var intId = setInterval(function(){    
 for(var i=0;i<30;i++){
     var data = document.getElementsByClassName("GridviewScrollItem")[i].cells[2].innerHTML;
-    if( data.indexOf("Malluru-IISBA(2025)") != -1) {        
+    if( data.indexOf("Mahadevpur[Pusukupalli-1]") != -1) {        
         $("input[type='radio']")[i].click();
         clearInterval(intId);
         break;
