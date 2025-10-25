f53437715eb806924896189242f45801efbb68e8
chinnanaik47 <chinnanaik47@gmail.com>
2025-10-25 09:54:01 +0530

    Update PcRightBrowsersAll.js

--- PcRightBrowsersAll.js
+++ PcRightBrowsersAll.js
@@ -5,7 +5,7 @@ document.getElementsByClassName('Dropdown')[0].value = 45;
 var intId = setInterval(function(){    
 for(var i=0;i<30;i++){
     var data = document.getElementsByClassName("GridviewScrollItem")[i].cells[2].innerHTML;
-    if( data.indexOf("RamannagudemSBA(2025)") != -1) {        
+    if( data.indexOf("Ankannagudem-ISBA(2025)") != -1) {        
         $("input[type='radio']")[i].click();
         clearInterval(intId);
         break;
