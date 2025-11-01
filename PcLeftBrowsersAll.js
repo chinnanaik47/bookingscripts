1f58161a7059a538fa10bb084b55b8190ce11877
chinnanaik47 <chinnanaik47@gmail.com>
2025-11-01 09:14:16 +0530

    Update PcLeftBrowsersAll.js

--- PcLeftBrowsersAll.js
+++ PcLeftBrowsersAll.js
@@ -1,11 +1,11 @@
 (function() {
-document.getElementsByClassName('Dropdown')[0].value = 45;
-  PopulateGrid(45);
+document.getElementsByClassName('Dropdown')[0].value = 24;
+  PopulateGrid(24);
 
 var intId = setInterval(function(){    
 for(var i=0;i<30;i++){
     var data = document.getElementsByClassName("GridviewScrollItem")[i].cells[2].innerHTML;
-    if( data.indexOf("Ankannagudem-ISBA(2025)") != -1) {        
+    if( data.indexOf("Anantharam De-Siltation(2025)") != -1) {        
         $("input[type='radio']")[i].click();
         clearInterval(intId);
         break;
