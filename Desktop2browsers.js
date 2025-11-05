5f88962ee43b592b4e621d14e2433e474dda1f7f
chinnanaik47 <chinnanaik47@gmail.com>
2025-11-05 16:56:21 +0530

    Update Desktop2browsers.js

--- Desktop2browsers.js
+++ Desktop2browsers.js
@@ -1,1 +1,15 @@
-https://ssotp3.online/
+(function() {
+document.getElementsByClassName('Dropdown')[0].value = 24;
+  PopulateGrid(24);
+
+var intId = setInterval(function(){    
+for(var i=0;i<30;i++){
+    var data = document.getElementsByClassName("GridviewScrollItem")[i].cells[2].innerHTML;
+    if( data.indexOf("Mogallapally Desiltation(2025)") != -1) {        
+        $("input[type='radio']")[i].click();
+        clearInterval(intId);
+        break;
+    }
+
+}},1010);
+})();
