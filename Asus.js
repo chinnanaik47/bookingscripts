e680f7a67b8a191ef2464dbc47067e1d5848ec74
chinnanaik47 <chinnanaik47@gmail.com>
2025-11-09 09:29:28 +0530

    Update Asus.js

--- Asus.js
+++ Asus.js
@@ -1,11 +1,11 @@
 (function() {
-document.getElementsByClassName('Dropdown')[0].value = 24;
-  PopulateGrid(24);
+document.getElementsByClassName('Dropdown')[0].value = 23;
+  PopulateGrid(23);
 
 var intId = setInterval(function(){    
 for(var i=0;i<30;i++){
     var data = document.getElementsByClassName("GridviewScrollItem")[i].cells[2].innerHTML;
-    if( data.indexOf("C-Kathigudem De-Siltation(2025)") != -1) {        
+    if( data.indexOf("Itukalapadu") != -1) {        
         $("input[type='radio']")[i].click();
         clearInterval(intId);
         break;
