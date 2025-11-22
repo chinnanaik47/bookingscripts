c110d0aa7f944d8285b0fcde5d8e74871882a836
chinnanaik47 <chinnanaik47@gmail.com>
2025-11-22 09:59:24 +0530

    Update PcLeftBrowsersAll.js

--- PcLeftBrowsersAll.js
+++ PcLeftBrowsersAll.js
@@ -1,11 +1,11 @@
 (function() {
-document.getElementsByClassName('Dropdown')[0].value = 24;
-  PopulateGrid(24);
+document.getElementsByClassName('Dropdown')[0].value = 20;
+  PopulateGrid(20);
 
 var intId = setInterval(function(){    
 for(var i=0;i<30;i++){
     var data = document.getElementsByClassName("GridviewScrollItem")[i].cells[2].innerHTML;
-    if( data.indexOf("Ramnagar (Aswapuram) De-Siltation (2025)") != -1) {        
+    if( data.indexOf("Challur") != -1) {        
         $("input[type='radio']")[i].click();
         clearInterval(intId);
         break;
