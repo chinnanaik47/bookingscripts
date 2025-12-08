3efc1222b33930e167d1b146228e6875df95dc8c
chinnanaik47 <chinnanaik47@gmail.com>
2025-12-08 09:27:04 +0530

    Update PcRightBrowsersAll.js

--- PcRightBrowsersAll.js
+++ PcRightBrowsersAll.js
@@ -5,7 +5,7 @@ document.getElementsByClassName('Dropdown')[0].value = 27;
 var intId = setInterval(function(){    
 for(var i=0;i<30;i++){
     var data = document.getElementsByClassName("GridviewScrollItem")[i].cells[2].innerHTML;
-    if( data.indexOf("Bommapur/2024/4") != -1) {        
+    if( data.indexOf("Kannepalli") != -1) {        
         $("input[type='radio']")[i].click();
         clearInterval(intId);
         break;
