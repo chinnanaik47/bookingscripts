0493906165f838361116ce281cb885c9d0d7f49a
chinnanaik47 <chinnanaik47@gmail.com>
2025-12-11 09:50:55 +0530

    Update PcRightBrowsersAll.js

--- PcRightBrowsersAll.js
+++ PcRightBrowsersAll.js
@@ -1,15 +1,15 @@
 (function() {
-document.getElementsByClassName('Dropdown')[0].value = 27;
-  PopulateGrid(27);
+document.getElementsByClassName('Dropdown')[0].value = 20;
+  PopulateGrid(20);
 
 var intId = setInterval(function(){    
 for(var i=0;i<30;i++){
     var data = document.getElementsByClassName("GridviewScrollItem")[i].cells[2].innerHTML;
-    if( data.indexOf("Kannepalli") != -1) {        
+    if( data.indexOf("Challur") != -1) {        
         $("input[type='radio']")[i].click();
         clearInterval(intId);
         break;
-    }
+    }
 
 }},1100);
 })();
